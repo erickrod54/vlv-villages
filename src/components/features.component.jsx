0f363aa96ce25c4a72b96ff373febd749d321f53
Erick Rodriguez <erickrod54@gmail.com>
2023-02-01 02:06:16 -0500

    vlv-villages version 1.04 - features.component - Features: Switching components from place to get the right aspect by layout

--- src/components/features.component.jsx
+++ src/components/features.component.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import FeaturesContent from "./features.content.component";
 import FeaturesText from "./features.text.component";
 
+/**vlv-villages version 1.04 - features.component -
+ * Features:
+ * 
+ *   --> Switching components from place to get 
+ *       the right aspect by layout.
+ * 
+ * Note: Changes will be done later in this 
+ * component
+ */
+
 const FeaturesComponent = () => {
 
     return(
         <>
-            <FeaturesText />
             <FeaturesContent />
+            <FeaturesText />
         </>
     )
 }
