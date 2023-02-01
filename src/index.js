dd7a3155aff75568eadcfe491daa891593ccf199
Erick Rodriguez <erickrod54@gmail.com>
2023-02-01 02:12:16 -0500

    vlv-villages version 1.04 - index js - Features: Wrapping the App with the VLVillagesProvider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { VLVillagesProvider } from './context';
+
+/**vlv-villages version 1.04 - index js - Features:
+ * 
+ *   --> Wrapping with App with the 
+ *       VLVillagesProvider
+ * 
+ * Note: pending to implemnt the navigation by 
+ * building each page in detail
+ */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <VLVillagesProvider>
+      <App />
+    </VLVillagesProvider>
   </React.StrictMode>
 );
 
