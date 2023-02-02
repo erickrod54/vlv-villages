5f07fcc668cf1ba51d1e4fc47b8816e0f5292af7
Erick Rodriguez <erickrod54@gmail.com>
2023-02-02 09:51:29 -0500

    vlv-villages version 1.05 - data js - Features: Building Images array with hero asset

---
+++ src/data.js
@@ -0,0 +1,17 @@
+import { hero } from "./assets/index.assets";
+
+/**vlv-villages version 1.05 - data js -
+ * Features:
+ * 
+ *   --> Building Images array with 'hero' asset.
+ * 
+ * Note: in this file will have place all data
+ * related with the app
+ */
+
+export const Images = [
+    {   
+        id: 1,
+        img: hero
+    },
+]
