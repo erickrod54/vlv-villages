998e7e8d63db94d41f8112c60efdf6738a6eb13b
Erick Rodriguez <erickrod54@gmail.com>
2023-02-02 09:48:55 -0500

    vlv-villages version 1.05 - index.assets - Features: Importing and exporting hero assets

---
+++ src/assets/index.assets.js
@@ -0,0 +1,12 @@
+import hero from './hero.jpg'
+
+/**vlv-villages version 1.05 - index.assets -
+ * Features:
+ * 
+ *   --> Importing and exporting 'hero' asset.
+ * 
+ * Note: Changes will be done later in this 
+ * component
+ */
+
+export { hero }
