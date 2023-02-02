dcde509a86af1e0203dc547a0844ac8ab06a32f0
Erick Rodriguez <erickrod54@gmail.com>
2023-02-02 09:50:02 -0500

    vlv-villages version 1.05 - index.pages - Features: Importing and exporting navigation pages

--- src/pages/index.pages.jsx
+++ src/pages/index.pages.jsx
@@ -1,11 +1,13 @@
+import AboutPage from "./about.page";
+import ContactPage from "./contact.page";
 import HomePage from "./home.page";
+import VillasPage from "./villas.page";
 
-/**vlv-villages version 1 - index.pages - Features:
+/**vlv-villages version 1.05 - index.pages - Features:
  * 
- *   --> Importing and exporting 'HomePage' 
- *       Page.
+ *   --> Importing and exporting navigation pages.
  * 
  * Note: First page page component
  */
 
-export { HomePage }
+export { HomePage, AboutPage, ContactPage, VillasPage }
