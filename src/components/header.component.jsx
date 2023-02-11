31533e64db93b1d55de20da69b6c020f6d8318ed
Erick Rodriguez <erickrod54@gmail.com>
2023-02-11 05:44:52 -0500

    vlv-villages version 1.09 - HeaderComponent - Features: Building all header content

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-/**vlv-villages version 1.07 - HeaderComponent -
+/**vlv-villages version 1.09 - HeaderComponent -
  * Features:
  * 
- *   --> Finishing context test
+ *   --> Building all header content.
  * 
  * Note: Pending to make it dynamic by implementing
  * an array with the links, and set the background 
@@ -38,6 +38,17 @@ const HeaderComponent = () => {
                     </div>
                 </div>
             </nav>
+            <div className="header_logo-content">
+                <a href="#" className="header_logo-content--logo">
+                    <span>V</span>L<span>V</span>
+                </a>
+
+                <div className="header_logo-content--content">
+                    <h1 className="header_logo-content--title">Relax your soul</h1>
+                    <p className="header_logo-content--para">The team of VLV welcomes you. Start relaxing your soul and enjoy your stay</p>
+                    <a href="#" className="header_logo-content--link">Discover more</a>
+                </div>
+            </div>
         </header>
     )
 }
