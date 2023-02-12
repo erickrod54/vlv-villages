56e62fd01db2667e0b8f259613d4d377ce416fbe
Erick Rodriguez <erickrod54@gmail.com>
2023-02-11 20:08:14 -0500

    vlv-villages version 1.11 - data js - Features: Building mainNavLinks for navigation links

--- src/data.js
+++ src/data.js
@@ -1,9 +1,9 @@
 import { hero } from "./assets/index.assets";
 
-/**vlv-villages version 1.05 - data js -
+/**vlv-villages version 1.11 - data js -
  * Features:
  * 
- *   --> Building Images array with 'hero' asset.
+ *   --> Building 'mainNavLinks' for navigation links.
  * 
  * Note: in this file will have place all data
  * related with the app
@@ -13,5 +13,24 @@ export const Images = [
     {   
         id: 1,
         img: hero
+    },
+]
+
+export const mainNavLinks = [
+    {
+        id:1,
+        linkname:'home'
+    },
+    {
+        id:2,
+        linkname:'villas'
+    },
+    {
+        id:3,
+        linkname:'about'
+    },
+    {
+        id:4,
+        linkname:'contact'
     },
 ]
