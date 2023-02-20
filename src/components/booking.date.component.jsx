ff08e973e47381cbcef7092e9811612c6e04faf5
Erick Rodriguez <erickrod54@gmail.com>
2023-02-20 13:26:12 -0500

    vlv-villages version 2.01 - BookingDate - Features: Destructuring BookingDateData and setting data on JSX

--- src/components/booking.date.component.jsx
+++ src/components/booking.date.component.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import { useVLVillagesContext } from "../context";
 
-/**vlv-villages version 2.00 - BookingDate - Features:
+/**vlv-villages version 2.01 - BookingDate - Features:
  * 
- *   --> Destructuring and testing 'BookingDateData'.
- *       from the context.
+ *   --> Destructuring 'BookingDateData' and setting 
+ *       variables.
  * 
  * Note: pending to create constants to place data in JSX
  */
@@ -17,47 +17,59 @@ const BookingDate = () => {
     console.log('this is then booking data ==>', BookingDateData)
 
     /**Arrival data */
+    const arrivalTitle = BookingDateData[0].title;
+    const arrivalMonth = BookingDateData[0].month;
+    const arrivalDay = BookingDateData[0].day;
 
     /**Departure data */
+    const departureTitle = BookingDateData[1].title;
+    const departureMonth = BookingDateData[1].month;
+    const departureDay = BookingDateData[1].day;
 
     /**Guest data */
+    const guestTitle = BookingDateData[2].title;
+    const guestNumber = BookingDateData[2].guestnumber;
 
     /**Reservation data */
+    const contactTitle = BookingDateData[3].title;
+    const contactPhonetitle = BookingDateData[3].phonetitle;
+    const contactPhone = BookingDateData[3].phone;
+    const contactReservation = BookingDateData[3].reservation;
 
     return(
         <section id="booking-date">
             <div className="booking-date_schedule">
                 <div className="booking-date_schedule--arrival">
-                    <h5>Arrival</h5>
+                    <h5>{arrivalTitle}</h5>
                     <div>
-                        <p>May</p>
-                        <p>09</p>
+                        <p>{arrivalMonth}</p>
+                        <p>{arrivalDay}</p>
                         <p>&#65088;</p>
                     </div>
                 </div>
                 <div className="booking-date_schedule--departure">
-                    <h5>Departures</h5>
+                    <h5>{departureTitle}</h5>
                     <div>
-                        <p>May</p>
-                        <p>23</p>
+                        <p>{departureMonth}</p>
+                        <p>{departureDay}</p>
                         <p>&#65088;</p>
                     </div>
                 </div>
                 <div className="booking-date_schedule--guests">
-                    <h5>Guests</h5>
+                    <h5>{guestTitle}</h5>
                     <div>
                         <p>&#65087;</p>
-                        <p>01</p>
+                        <p>{guestNumber}</p>
                         <p>&#65088;</p>
                     </div>
                 </div>
             </div>
         
             <div className="booking-date_contact">
-                <Link to='#' className="booking-date_contact--link">Make a Reservation</Link>
-                <p>Reservation Hotline</p>
-                <p>+1-5263-8965-6547</p>
-                <p>Reservation Assistance Available 24 hours</p>
+                <Link to='#' className="booking-date_contact--link">{contactTitle}</Link>
+                <p>{contactPhonetitle}</p>
+                <p>{contactPhone}</p>
+                <p>{contactReservation}</p>
             </div>
         </section>
     )
