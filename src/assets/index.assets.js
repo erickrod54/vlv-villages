f1508029810940d434228fe8b58999f2741fdc14
Erick Rodriguez <erickrod54@gmail.com>
2023-02-23 01:36:19 -0500

    vlv-villages version 2.08 - index.assets - Features: Importing and exporting villas assets for villas section

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -1,12 +1,19 @@
-import hero from './hero.jpg'
+import hero from './hero.jpg';
+import villa1 from './Villas/villa-1.jpg';
+import villa2 from './Villas/villa-2.jpg';
+import villa3 from './Villas/villa-3.jpg';
+import villa4 from './Villas/villa-4.jpg';
+import villa5 from './Villas/villa-5.jpg';
+import villa6 from './Villas/villa-6.jpg';
 
-/**vlv-villages version 1.05 - index.assets -
+/**vlv-villages version 2.08 - index.assets -
  * Features:
  * 
- *   --> Importing and exporting 'hero' asset.
+ *   --> Importing and exporting 'villa' assets
+ *       for villas section.
  * 
  * Note: Changes will be done later in this 
  * component
  */
 
-export { hero }
+export { hero, villa1, villa2, villa3, villa4, villa5, villa6 }
