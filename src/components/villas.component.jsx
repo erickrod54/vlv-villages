40d5a4fc43fcbe1fe548e0a904517ead1f131d7f
Erick Rodriguez <erickrod54@gmail.com>
2023-02-24 02:39:34 -0500

    vlv-villages version 2.10 - VillasComponent - Features: Destrcuturing villasBackgrounds, Keeping villa1 to villa6 and drilling them to set the image background

--- src/components/villas.component.jsx
+++ src/components/villas.component.jsx
@@ -3,20 +3,33 @@ import { Link } from 'react-router-dom';
 import styled from "styled-components";
 import { useVLVillagesContext } from "../context";
 
-/**vlv-villages version 2.09 - VillasComponent 
+/**vlv-villages version 2.10 - VillasComponent 
  * - Features:
  * 
- *   --> Migrating styles to it's own style 
- *       Component.  
+ *   --> Destructuring 'villasBackgorunds'
  * 
- * Note: Pending to fix background images.
+ *   --> Keeping 'villa1' to 'villa6' images 
+ *       and drilling them to set the background.  
+ * 
+ * Note: the background image will be set to 'url'
+ * as follows:
+ * 
+ *      'url(${({villa4}) => villa4 });'
  */
 
 const VillasComponent = () => {
 
-    const { villasData } = useVLVillagesContext()
+    const { villasData, villasBackgorunds } = useVLVillagesContext()
 
-    console.log('the villas data ==>', villasData)
+    /**  console.log('the villas data ==>', villasData)
+    console.log('the villas backgrounds', villasBackgorunds) */
+
+    const villa1 = villasBackgorunds[0].image;
+    const villa2 = villasBackgorunds[1].image;
+    const villa3 = villasBackgorunds[2].image;
+    const villa4 = villasBackgorunds[3].image;
+    const villa5 = villasBackgorunds[4].image;
+    const villa6 = villasBackgorunds[5].image;
 
     return(
         <section id="villas">
@@ -24,9 +37,9 @@ const VillasComponent = () => {
            {villasData.map((villa) => {
             const { id, villaname, price, description } = villa;
             return(
-                <VillasWrapper>
+                <VillasWrapper key={id} villa1={villa1} villa2={villa2} villa3={villa3} villa4={villa4} villa5={villa5} villa6={villa6}>
 
-                <div key={id} className={`villas-${id}`}>
+                <div className={`villas-${id}`}>
                     <h2 className={`villas-${id}--title`}>{villaname}</h2>
                     <p>{description}</p>
                     <Link to='#' className={`villas-${id}--link`}>{price}</Link>
@@ -44,8 +57,8 @@ const VillasWrapper = styled.div`
     /** Caret selector -to select all villas- -*/
     div[class^=villas]{
     padding: 2rem;
-    /*background-position: center;*/
-    /*background-size: cover;**/
+    background-position: center;
+    background-size: cover;
     transition: transform var(--transition-duration) ease-in-out;
     }
 
@@ -100,13 +113,13 @@ const VillasWrapper = styled.div`
     }
 
     /*effect of overflowing the button -- start */
-    div[class^=villas]:nth-child(1n) a:link::before,
-    div[class^=villas]:nth-child(1n) a:visited::before{
+    div[class^=villas]:nth-child(1n) a[class^=villas]:link::before,
+    div[class^=villas]:nth-child(1n) a[class^=villas]:visited::before{
     transform: translate(-50%, -50%) rotate(45deg);
     }
 
-    div[class^=villas]:nth-child(2n) a:link::before,
-    div[class^=villas]:nth-child(2n) a:visited::before{
+    div[class^=villas]:nth-child(2n) a[class^=villas]:link::before,
+    div[class^=villas]:nth-child(2n) a[class^=villas]:visited::before{
     transform: translate(-50%, -50%) rotate(-45deg);
     }
 
@@ -117,49 +130,46 @@ const VillasWrapper = styled.div`
     height: 400%;
     }
 
-    /*background-position: center;
-    background-size: cover;**/
-
     .villas-1{
     background: linear-gradient(
         var(--color-primary-light),
         var(--color-tertiary-light)),
-        url('./assets/Villas/villa-1.jpg');
+        url(${({villa1}) => villa1 });
     }
 
     .villas-2{
     background: linear-gradient(
         var(--color-primary-light),
         var(--color-tertiary-light)),
-        url('./assets/Villas/villa-2.jpg');
+        url(${({villa2}) => villa2 });
     }
 
     .villas-3{
     background: linear-gradient(
         var(--color-primary-light),
         var(--color-tertiary-light)),
-        url('./assets/Villas/villa-3.jpg');
+        url(${({villa3}) => villa3 });
     }
 
     .villas-4{
     background: linear-gradient(
         var(--color-primary-light),
         var(--color-tertiary-light)),
-        url('./assets/Villas/villa-4.jpg');
+        url(${({villa4}) => villa4 });
     }
 
     .villas-5{
     background: linear-gradient(
         var(--color-primary-light),
         var(--color-tertiary-light)),
-        url('./assets/Villas/villa-5.jpg');
+        url(${({villa5}) => villa5 });
     }
 
     .villas-6{
     background: linear-gradient(
         var(--color-primary-light),
         var(--color-tertiary-light)),
-        url('./assets/Villas/villa-6.jpg');
+        url(${({villa6}) => villa6 });
     }
 `
 
