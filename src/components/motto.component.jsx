abad8b7a1f01d40a7ad26a506da9c6fae111d6b7
Erick Rodriguez <erickrod54@gmail.com>
2023-02-24 10:07:53 -0500

    vlv-villages version 2.11 - MottoComponent - Features: Building motto basic Component

--- src/components/motto.component.jsx
+++ src/components/motto.component.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 
+/**vlv-villages version 2.11 - MottoComponent 
+ * - Features:
+ * 
+ *   --> Building 'motto' basic Component.  
+ * 
+ * Note: Pending to separate the data from the markup.
+ */
+
 const MottoComponent = () => {
 
     return(
         <section id="motto">
-            Motto Component
+            <div className="motto-content">
+                <h2 className="motto-content--title">
+                    We Imagine Luxury
+                </h2>
+                <p className="motto-content--para">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut consequat semper viverra nam libero justo. Leo vel orci porta non pulvinar neque laoreet suspendisse
+                </p>
+            </div>
         </section>
     )
 }
