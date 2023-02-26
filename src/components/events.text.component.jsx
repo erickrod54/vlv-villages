cd9e25846f069fc14c7825cc96838fc8893c4710
Erick Rodriguez <erickrod54@gmail.com>
2023-02-25 20:22:58 -0500

    vlv-villages version 2.14 - EventsContent - Features: Building EventsText basic Component

--- src/components/events.text.component.jsx
+++ src/components/events.text.component.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 
+/**vlv-villages version 2.14 - EventsContent 
+ * - Features:
+ * 
+ *   --> Building 'EventsText' basic Component.  
+ * 
+ * Note: Pending to separate the data from the markup.
+ */
+
 const EventsText = () => {
 
     return(
         <section id="events-text">
-            Events Text
+            <div className="events-text-content">
+                <h2 className="events-text-content--title">
+                    Events
+                </h2>
+            </div>
         </section>
     )
 }
