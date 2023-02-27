5111b806863817338cfd164432926c415b9f2338
Erick Rodriguez <erickrod54@gmail.com>
2023-02-27 12:49:03 -0500

    vlv-villages version 2.17 - EventsText - Features: Destructuring and placing GalleryTextTitle data in the markup

--- src/components/gallery.text.component.jsx
+++ src/components/gallery.text.component.jsx
@@ -1,20 +1,25 @@
 import React from "react";
+import { useVLVillagesContext } from "../context";
 
-/**vlv-villages version 2.16 - EventsText 
+/**vlv-villages version 2.17 - EventsText 
  * - Features:
  * 
- *   --> Building Basic 'GalleryText' Component.  
+ *   --> Destructuring and placing 'GalleryTextTitle'
+ *       dat in the markup.  
  * 
  * Note: Pending to separate the data from the markup.
  */
 
+
 const GalleryText = () => {
+
+    const { GalleryTextTitle } = useVLVillagesContext()
 
     return(
         <section id="gallery-text">
         <div className="gallery-text-content">
             <h2 className="gallery-text-content--title">
-              Gallery  
+                {GalleryTextTitle}
             </h2>
         </div>
     </section>
