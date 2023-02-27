9c7f80e5efb873f43b864549f1528522723c68ac
Erick Rodriguez <erickrod54@gmail.com>
2023-02-26 20:28:01 -0500

    vlv-villages version 2.16 - EventsText - Features: Building Basic GalleryText Component

--- src/components/gallery.text.component.jsx
+++ src/components/gallery.text.component.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
+/**vlv-villages version 2.16 - EventsText 
+ * - Features:
+ * 
+ *   --> Building Basic 'GalleryText' Component.  
+ * 
+ * Note: Pending to separate the data from the markup.
+ */
+
 const GalleryText = () => {
 
     return(
         <section id="gallery-text">
-            Gallery Text
-        </section>
+        <div className="gallery-text-content">
+            <h2 className="gallery-text-content--title">
+              Gallery  
+            </h2>
+        </div>
+    </section>
     )
 }
 
