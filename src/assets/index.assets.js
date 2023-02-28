d4f5c631eecbfd0df1b317031623c7cd880cfcc0
Erick Rodriguez <erickrod54@gmail.com>
2023-02-28 12:47:41 -0500

    vlv-villages version 2.18 - index.assets  - Features: Importing and exporting 10 gallery assets for gallery carrousel section

--- src/assets/index.assets.js
+++ src/assets/index.assets.js
@@ -5,15 +5,25 @@ import villa3 from './Villas/villa-3.jpg';
 import villa4 from './Villas/villa-4.jpg';
 import villa5 from './Villas/villa-5.jpg';
 import villa6 from './Villas/villa-6.jpg';
+import gallery1 from './Gallery/gallery-1.jpg';
+import gallery2 from './Gallery/gallery-2.jpg';
+import gallery3 from './Gallery/gallery-3.jpg';
+import gallery4 from './Gallery/gallery-4.jpg';
+import gallery5 from './Gallery/gallery-5.jpg';
+import gallery6 from './Gallery/gallery-6.jpg';
+import gallery7 from './Gallery/gallery-7.jpg';
+import gallery8 from './Gallery/gallery-8.jpg';
+import gallery9 from './Gallery/gallery-9.jpg';
+import gallery10 from './Gallery/gallery-10.jpg';
 
-/**vlv-villages version 2.08 - index.assets -
+/**vlv-villages version 2.18 - index.assets -
  * Features:
  * 
- *   --> Importing and exporting 'villa' assets
- *       for villas section.
+ *   --> Importing and exporting 10 'gallery' assets
+ *       for 'gallery-carrouosel' section.
  * 
  * Note: Changes will be done later in this 
  * component
  */
 
-export { hero, villa1, villa2, villa3, villa4, villa5, villa6 }
+export { hero, villa1, villa2, villa3, villa4, villa5, villa6, gallery1, gallery2, gallery3, gallery4, gallery5, gallery6, gallery7, gallery8, gallery9, gallery10 }
