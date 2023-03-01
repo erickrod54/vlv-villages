ac88062fe20bf880107f54a071a3cea4dd52f373
Erick Rodriguez <erickrod54@gmail.com>
2023-03-01 00:56:31 -0500

    vlv-villages version 2.20 - GalleryCarousel - Features: Finishing Markup for GalleryCarousel

--- src/components/gallery.carousel.component.jsx
+++ src/components/gallery.carousel.component.jsx
@@ -1,36 +1,59 @@
 import React from "react";
 import { useVLVillagesContext } from "../context";
 
-/**vlv-villages version 2.18 - GalleryCarousel -
+/**vlv-villages version 2.20 - GalleryCarousel -
  * Features:
  * 
- *   --> Building basic 'GalleryCarousel' component.
+ *   --> Finishing Markup for 'GalleryCarousel'. 
  * 
  * Note: Motto data will be use for Motto Component.
  */
 
 const GalleryCarousel = () => {
     
-    const { galleryCarrouselData } = useVLVillagesContext()
+    const { galleryCarrouselData, galleryCarrouselImages } = useVLVillagesContext()
 
-    console.log('galleryCarrouselData ==>', galleryCarrouselData)
+    //console.log('galleryCarrouselData ==>', galleryCarrouselData)
+
+    console.log('galleryCarrouselImages ==>', galleryCarrouselImages)
 
     const arrowLeft = galleryCarrouselData[0].icon;
     const arrowRight = galleryCarrouselData[1].icon;
 
     return(
         <section id="gallery-carousel">
-            <div className="gallery-carousel--btn gallery-carousel--btn--left">
+            <button className="gallery-carousel--btn gallery-carousel--btn--left">
                 {arrowLeft}
-            </div>
+            </button>
             <div className="gallery-carousel--image-container">
                 <ul className="gallery-carousel--image-container--list">
-                    <li className="gallery-carousel--image-container--list--item">
-                        here is going to go the images
-                    </li>
+                    {galleryCarrouselImages.map((picture) => {
+                        const { id, image } = picture;
+
+                        return(
+                            <li key={id} className="gallery-carousel--image-container--list--item">
+                                <img src={image} alt='villa'/>
+                            </li>
+                        )
+                    })}
                 </ul>
             </div>
-            <div></div>
+            <button className="gallery-carousel--btn gallery-carousel--btn--right">
+                {arrowRight}
+            </button>
+            <div className="gallery-carousel--nav">
+                <button className="gallery-carousel-nav--btn current--img"></button>
+                <button className="gallery-carousel-nav--btn">12</button>
+                <button className="gallery-carousel-nav--btn">13</button>
+                <button className="gallery-carousel-nav--btn">14</button>
+                <button className="gallery-carousel-nav--btn">15</button>
+                <button className="gallery-carousel-nav--btn"></button>
+                <button className="gallery-carousel-nav--btn"></button>
+                <button className="gallery-carousel-nav--btn"></button>
+                <button className="gallery-carousel-nav--btn"></button>
+                <button className="gallery-carousel-nav--btn"></button>
+                <button className="gallery-carousel-nav--btn"></button>
+            </div>
         </section>
     )
 }
