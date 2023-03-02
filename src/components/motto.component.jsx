61da5c841c5987a7becda16544d605b5ad0a65c5
Erick Rodriguez <erickrod54@gmail.com>
2023-03-02 12:31:55 -0500

    vlv-villages version 3.01 - MottoComponent - Features: Building Motto style Component

--- src/components/motto.component.jsx
+++ src/components/motto.component.jsx
@@ -1,12 +1,11 @@
 import React from "react";
+import styled from "styled-components";
 import { useVLVillagesContext } from "../context";
 
-/**vlv-villages version 2.13 - MottoComponent 
+/**vlv-villages version 3.01 - MottoComponent 
  * - Features:
  * 
- *   --> Destruturing 'MottoData' from the context.
- * 
- *   --> Placing data in the markup.  
+ *   --> Building 'Motto' style Component.  
  * 
  * Note: Pending to separate the data from the markup.
  */
@@ -22,6 +21,7 @@ const MottoComponent = () => {
 
     return(
         <section id="motto">
+            <MottoWrapper>
             <div className="motto-content">
                 <h2 className="motto-content--title">
                     {title}
@@ -30,8 +30,39 @@ const MottoComponent = () => {
                 {description}
                 </p>
             </div>
+            </MottoWrapper>
         </section>
     )
 }
+
+const MottoWrapper = styled.div`
+    /*will center all the content*/
+    .motto-content{
+    width: 80%;
+    margin: 0 auto;
+    text-align: center;
+    }
+
+    .motto-content--title{
+    font-size: var(--font-size-title);
+    padding: 0 2rem 1rem;
+    font-weight: 300;
+    color: white;
+    text-transform: uppercase;
+    margin-bottom: 4rem;
+    position: relative;
+    }
+
+    /*line below the title**/
+    .motto-content--title::after{
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 45%;
+    height: 0.2rem;
+    width: 10%;
+    background-color: white;
+}
+`
 
 export default MottoComponent;
