a54f74ca1da429b0a1f320299d3a5620ba7fb8f9
Erick Rodriguez <erickrod54@gmail.com>
2023-03-04 21:00:35 -0500

    vlv-villages version 3.04 - GalleryCarousel - Features: Building the jsx list with the images and converting it into an Array, Selecting multipole jsx elements from the Carousel

--- src/components/gallery.carousel.component.jsx
+++ src/components/gallery.carousel.component.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useVLVillagesContext } from "../context";
 
-/**vlv-villages version 2.20 - GalleryCarousel -
+import '../utils';
+
+/**vlv-villages version 3.04 - GalleryCarousel -
  * Features:
  * 
- *   --> Finishing Markup for 'GalleryCarousel'. 
+ *   --> Building the jsx 'list' with the images
+ *       and converting it into an Array.
+ * 
+ *   --> Selecting multiple jsx elements from the 
+ *       Carousel. 
  * 
  * Note: Motto data will be use for Motto Component.
  */
@@ -12,18 +18,51 @@ import { useVLVillagesContext } from "../context";
 const GalleryCarousel = () => {
     
     const { galleryCarrouselData, galleryCarrouselImages } = useVLVillagesContext()
-
+    
     //console.log('galleryCarrouselData ==>', galleryCarrouselData)
 
-    console.log('galleryCarrouselImages ==>', galleryCarrouselImages)
-
+    //console.log('galleryCarrouselImages ==>', galleryCarrouselImages)
+    
     const arrowLeft = galleryCarrouselData[0].icon;
     const arrowRight = galleryCarrouselData[1].icon;
+    
+    const list = () => {
+        const listGallery = document.querySelector('.gallery-carousel--image-container--list');
+        const styleClassChildren = listGallery.querySelectorAll('.gallery-carousel--image-container--list--item');
+        /**here iu convert the NodeList in an array */
+        const imgs = Array.from(styleClassChildren)
 
+        const nextButton = document.querySelector(".gallery-carousel--btn--right");
+        const prevButton = document.querySelector(".gallery-carousel--btn--left")
+        const CarouselNav = document.querySelector(".gallery-carousel--nav");
+        const dots = Array.from(CarouselNav.children)
+        
+        /**this is the list - is working*/
+        /**so far jsx items as been selected */
+
+        //console.log(imgs);
+
+        /**buttons been selected */
+        //console.log(nextButton);
+        //console.log(prevButton);
+
+        /**carousel nav */
+        //console.log(CarouselNav)
+
+        /**the dots */
+        //console.log(dots)
+
+    } 
+    
+    useEffect(() => {
+        list()
+    },[])
+    
+    
     return(
         <section id="gallery-carousel">
             <button className="gallery-carousel--btn gallery-carousel--btn--left">
-                {arrowLeft}
+                <i>{arrowLeft}</i>
             </button>
             <div className="gallery-carousel--image-container">
                 <ul className="gallery-carousel--image-container--list">
@@ -39,7 +78,7 @@ const GalleryCarousel = () => {
                 </ul>
             </div>
             <button className="gallery-carousel--btn gallery-carousel--btn--right">
-                {arrowRight}
+                <i>{arrowRight}</i>
             </button>
             <div className="gallery-carousel--nav">
                 <button className="gallery-carousel-nav--btn current--img"></button>
