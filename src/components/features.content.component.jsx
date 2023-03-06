7f80b7ab251d2277ce708de0fd07a491bb4c72f7
Erick Rodriguez <erickrod54@gmail.com>
2023-03-06 09:35:45 -0500

    vlv-villages version 3.12 - FeaturesContent - Features: Building markup for FeaturesContent

--- src/components/features.content.component.jsx
+++ src/components/features.content.component.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-/**vlv-villages version 1.04 - FeaturesContent  -
+/**vlv-villages version 3.12 - FeaturesContent  -
  * Features:
  * 
- *   --> Changing clasName by id.
+ *   --> Building markup for 'FeaturesContent'.
  * 
  * Note: Changes will be done later in this 
  * component
@@ -14,7 +14,23 @@ const FeaturesContent = () => {
 
     return(
         <section id="features-content">
-            Features Content
+            <div className="features-content--feature--1">
+                <h2 className="features-content--feature--1--title">
+                    Cocktail Package
+                </h2>
+                <p className="features-content--feature--1--para">
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut consequat semper viverra nam libero justo. Leo vel orci porta non pulvinar neque laoreet suspendisse
+                </p>
+            </div>
+
+            <div className="features-content--feature--2">
+                <h2 className="features-content--feature--2--title">
+                    Surfing Package
+                </h2>
+                <p className="features-content--feature--2--para">
+                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut consequat semper viverra nam libero justo. Leo vel orci porta non pulvinar neque laoreet suspendisse
+                </p>
+            </div>
         </section>
     )
 }
