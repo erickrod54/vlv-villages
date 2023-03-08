aa74f4016cdc5b13194bb0e10b067fc265f4a66e
Erick Rodriguez <erickrod54@gmail.com>
2023-03-08 01:00:10 -0500

    vlv-villages version 3.17 - Testimonials - Features: Destructuring testimonialsData, Placing Dymanicly testimonialsData in the Markup

--- src/components/testimonials.section.jsx
+++ src/components/testimonials.section.jsx
@@ -1,28 +1,41 @@
 import React from "react";
+import { useVLVillagesContext } from "../context";
 
-
-/**vlv-villages version 3.15 - Testimonials -
+/**vlv-villages version 3.17 - Testimonials -
  * Features:
  * 
- *   --> Building Markup for 'Testimonials'.
+ *   --> Destructuiring 'testimonialsData' 
+ *       from the context.
  * 
- * Note: Changes will be done later in this 
- * component
+ *   --> Placing Dyinamicly 'testimonialsData' 
+ *       in the Markup.   
+ * 
+ * Note: Pending to bring the styles, make the markup
+ * and set backgrounds
  */
 
 
 const Testimonials = () => {
 
+    const { testimonialsData } = useVLVillagesContext();
+    //console.log('this is the cards data =>', testimonialsData)
+    
     return(
         <section id="testimonials">
-            <div className="testimonials-card">
-                <div className="testimonials-card--front">
-                    <h2>Clark Lois</h2>
+            
+            {testimonialsData.map((card) => {
+                const { id, title, description } = card
+                return(
+                <div key={id} className="testimonials-card">
+                    <div className="testimonials-card--front">
+                    <h2>{title}</h2>
+                    </div>
+                    <div className="testimonials-card--back">
+                        <p>{description}</p>
+                    </div>
                 </div>
-                <div className="testimonials-card--back">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
-            </div>
+                )
+            })}
         </section>
     )
 }
