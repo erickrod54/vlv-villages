245887187865d951c78e3a65455d57e566f14529
Erick Rodriguez <erickrod54@gmail.com>
2023-03-09 00:44:19 -0500

    vlv-villages version 3.19 - FooterComponent - Features: Building the Markup for FooterComponent

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
-/**vlv-villages version 1.04 - FooterComponent -
+/**vlv-villages version 3.19 - FooterComponent -
  * Features:
  * 
- *   --> Changing clasName by id.
+ *   --> Building the Markup for 'FooterComponent'.
  * 
  * Note: Changes will be done later in this 
  * component
@@ -13,7 +14,21 @@ const FooterComponent = () => {
 
     return(
         <section id="footer">
-            Footer
+            <p>All Rights Reserved. &copy; VLV</p>
+            <div className="footer-social">
+                <Link to='#' className="footer-social--link">
+                    facebook
+                </Link>
+                <Link to='#' className="footer-social--link">
+                    instagram   
+                </Link>
+                <Link to='#' className="footer-social--link">
+                    twitter         
+                </Link>
+                <Link to='#' className="footer-social--link">
+                    linkedin         
+                </Link>
+            </div>
         </section>
     )
 }
