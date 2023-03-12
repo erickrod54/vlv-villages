6534d15f4a58b729e17f4b257b1869b2331a2dd2
Erick Rodriguez <erickrod54@gmail.com>
2023-03-11 21:41:09 -0500

    vlv-villages version 4.01 - index css - Features: Migrating Footer Styles to its own Component

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -1,28 +1,29 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import styled from "styled-components";
 import { useVLVillagesContext } from "../context";
 
-/**vlv-villages version 4.00 - FooterComponent -
+/**vlv-villages version 4.01 - FooterComponent -
  * Features:
  * 
- *   --> Destructuring 'footerBrandIcons' and 
- *       placing icons.
+ *   --> Building 'FooterWrapper' and 
+ *       placing footer styles and animations.
  * 
- * Note: pending to change the name of the data icons
+ * Note: pending to make dynamic data
  */
 
 const FooterComponent = () => {
-
+    
     const { footerBrandIcons } = useVLVillagesContext()
     console.log('this is the data for footerBrandIcons ==>',footerBrandIcons)
-
+    
     const facebook  = footerBrandIcons[0].imagesbkg
     const instagram = footerBrandIcons[1].imagesbkg
     const twitter   = footerBrandIcons[2].imagesbkg
     const linkedin  = footerBrandIcons[3].imagesbkg
-
+    
     return(
-        <section id="footer">
+        <FooterWrapper id="footer">
             <p>All Rights Reserved. &copy; VLV</p>
             <div className="footer-social">
                 <Link to='#' className="footer-social--link">
@@ -38,8 +39,62 @@ const FooterComponent = () => {
                     <i>{linkedin}</i>         
                 </Link>
             </div>
-        </section>
+        </FooterWrapper>
     )
 }
+
+const FooterWrapper = styled.section`
+    .footer-social--link:link,
+    .footer-social--link:visited{
+        margin-top: 4rem;
+        display: inline-block;
+        background-color: var(--color-secondary);
+        border-radius: 4rem;
+        margin-right: 1rem;
+        box-shadow: 0.5rem 0.5rem 2rem 0.5rem var(--color-secondary-light), -0.5rem -0.5rem 2rem 0.5rem var(--color-secondary-light);
+        transform: scale(0.8);
+        animation: social 3s linear infinite;
+    }
+
+    @keyframes social {
+    0%{
+        opacity: 0;
+    }
+
+    50%{
+        opacity: 1;
+        transform: scale(1.2);
+    }
+
+    100%{
+        opacity: 0;
+    }
+    }
+
+    .footer-social--link i{
+        font-size: 2rem;
+        color: var(--color-primary);
+    }
+
+    .footer-social--link:link:nth-child(1),
+    .footer-social--link:visited:nth-child(1){
+        animation-delay: 0s;
+    }
+
+    .footer-social--link:link:nth-child(2),
+    .footer-social--link:visited:nth-child(2){
+        animation-delay: 0.3s;
+    }
+
+    .footer-social--link:link:nth-child(3),
+    .footer-social--link:visited:nth-child(3){
+    animation-delay: 0.6s;
+    }
+
+    .footer-social--link:link:nth-child(4),
+    .footer-social--link:visited:nth-child(4){
+        animation-delay: 0.9s;
+    }
+`
 
 export default FooterComponent;
