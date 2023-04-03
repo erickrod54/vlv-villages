3e43aee0c5b60c9ace72e2285f14798d641aaa36
Erick Rodriguez <erickrod54@gmail.com>
2023-04-03 13:27:55 -0400

    vlv-villages version 4.07 - EventsContent: Placing data dynamicly in the Markup

--- src/components/events.content.component.jsx
+++ src/components/events.content.component.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { useVLVillagesContext } from "../context";
 
-/**vlv-villages version 4.06 - EventsContent 
+/**vlv-villages version 4.07 - EventsContent 
  * - Features:
  * 
- *   --> Destructuring 'EventsContentData' and
- *       placing it in the Markup.  
+ *   --> Placing data dynamicly in the Markup.  
  * 
  * Note: Pending to separate the data from the markup.
  */
@@ -15,29 +14,22 @@ const EventsContent = () => {
     const { EventsContentData } = useVLVillagesContext();
     console.log('EventsContentData ==>', EventsContentData)
 
-    const title1 = EventsContentData[0].title;
-    const description1 = EventsContentData[0].description;
-    const title2 = EventsContentData[1].title;
-    const description2 = EventsContentData[1].description;
-
     return(
         <section id="events-content">
-            <div className="events-content--event-1">
-                <h2 className="events-content--event-1--title">
-                    {title1}
-                </h2>
-                <p className="events-content--event-1--para">
-                 {description1}
-                </p>
-            </div>
-            <div className="events-content--event-2">
-                <h2 className="events-content--event-2--title">
-                    {title2}
-                </h2>
-                <p className="events-content--event-2--para">
-                 {description2}
-                </p>
-            </div>
+            {EventsContentData.map((eventcontent) => {
+                const { id, title, description } = eventcontent;
+
+                return(
+                <div key={id} className={`events-content--event-${id}`}>
+                    <h2 className={`events-content--event-${id}--title`}>
+                        {title}
+                    </h2>
+                    <p className={`events-content--event-${id}--para`}>
+                     {description}
+                    </p>
+                </div>            
+                )
+            })}
         </section>
     )
 }
