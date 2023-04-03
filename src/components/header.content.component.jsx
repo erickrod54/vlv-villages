18052fd8ecf19a18dd3d8a1960bc5aa8663ad28f
Erick Rodriguez <erickrod54@gmail.com>
2023-04-03 13:29:58 -0400

    vlv-villages version 4.07 - HeaderContent - Features: Destrcuturing HeaderData from  the context

--- src/components/header.content.component.jsx
+++ src/components/header.content.component.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { useVLVillagesContext } from "../context";
 
-/**vlv-villages version 1.14 - HeaderContent -
+/**vlv-villages version 4.07 - HeaderContent -
  * Features:
  * 
- *   --> Building 'HeaderContent'.
- * 
- *   --> Chyanging anchor tag for 'Link' from
- *       'router-react-dom'
+ *   --> Destructuring 'HeaderData' from the context.
  * 
  * Note: Pending to make it dynamic by implementing
  * an array with the links, and set the background 
@@ -16,11 +14,18 @@ import { Link } from 'react-router-dom';
 
 const HeaderContent = () => {
 
+    const { HeaderData } = useVLVillagesContext();
+    console.log('HeaderData ==> ', HeaderData);
+
+    const title = HeaderData[0].text
+    const description = HeaderData[1].text
+    const buttontext = HeaderData[2].text
+
     return(
     <div className="header_logo-content--content">
-        <h1 className="header_logo-content--title">Relax your soul</h1>
-        <p className="header_logo-content--para">The team of VLV welcomes you. Start relaxing your soul and enjoy your stay</p>
-        <Link to='#' className="header_logo-content--link">Discover more</Link>
+        <h1 className="header_logo-content--title">{title}</h1>
+        <p className="header_logo-content--para">{description}</p>
+        <Link to='#' className="header_logo-content--link">{buttontext}</Link>
     </div>     
     )
 }
